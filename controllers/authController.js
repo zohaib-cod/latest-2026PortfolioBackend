5a5e1454dd684f56de6b437400d3864d0f935ec8
ohaib-cod <alizohaib.web@gmail.com>
2026-09-23 04:40:41 +0500

    2

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,7 +28,12 @@ const loginUser = async (req, res) => {
       res.status(401).json({ message: 'Invalid email or password' });
     }
   } catch (error) {
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ 
+      message: 'Server error', 
+      error: error.message,
+      stack: error.stack,
+      jwtExists: !!process.env.JWT_SECRET
+    });
   }
 };
 
