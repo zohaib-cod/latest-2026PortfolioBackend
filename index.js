175b567a0311d46a14dd272e78155366f5fef6fe
ohaib-cod <alizohaib.web@gmail.com>
2026-09-23 04:15:06 +0500

    SERVER issue

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ const PORT = process.env.PORT || 5000;
 
 // Only listen locally, Vercel will handle the rest via module.exports
 if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => console.log(\`Server running locally on port \${PORT}\`));
+  app.listen(PORT, () => console.log(`Server running locally on port ${PORT}`));
 }
 
 module.exports = app;
